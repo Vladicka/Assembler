e745d2e275ee459b31a5396b176d1e4878b5b09d
Vladicka <vladicka7874@gmail.com>
2017-09-26 18:05:59 +0500

    Update Lab_4_3.asm

--- Lab 4/Lab_4_3.asm
+++ Lab 4/Lab_4_3.asm
@@ -3,18 +3,18 @@ start:
     mov ah, 0
     mov al, 10h
     int 10h
-    push   es
-    xor   ax,ax
-    mov   es,ax
-    mov   ah,6
-    mov   al,0
-    mov   bh,15
-    xor   cx,cx
-    mov   dh,es:[484h]
-    mov   dl,es:[44Ah]
-    dec   dl
-    int   10h
-    pop   es
+    push es
+    xor ax, ax
+    mov es, ax
+    mov ah, 6
+    mov al, 0
+    mov bh, 15
+    xor cx, cx
+    mov dh, es:[484h]
+    mov dl, es:[44Ah]
+    dec dl
+    int 10h
+    pop es
     mov ax, 4c00h
     int 21h
 code ends
